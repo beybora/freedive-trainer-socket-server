0381a7a74768309a6742f8dbc92e441641afe859
borabey <bora.b.sarikaya@gmail.com>
2024-07-03 17:07:43 +0200

    initialize cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv/config");
 const express = require("express");
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = createServer(app);
